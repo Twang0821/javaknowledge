4eda1e4df526cc66f2c6b8414e3ab6254b6e3dff
wangrentao <lyaning13142011@sina.com>
2019-05-02 22:49:07 +0800

    补充第一个java程序

---
+++ src/main/java/cn/twang/afirstjava/Demo.java
@@ -0,0 +1,21 @@
+package cn.twang.afirstjava;	//Demo类在cn.twang.demo包下
+
+
+/**
+ * @ClassName: Demo   
+ * @Description:    
+ * @author: Tyanao
+ * @date: 2016年4月2日 下午10:44:27     
+ * @Copyright: 2016 www.tydic.com Inc. All rights reserved. 
+ * 注意：本内容仅限于****内部传阅，禁止外泄以及用于其他的商业目
+ */
+public class Demo {
+	
+	public static void main(String[] args) {
+		System.out.println("helloword.java");
+		
+		//调用其它包中的类,则在上面要引入此类所在的包名直到所在java文件的类名。如:import cn.twang.utils.Utils
+		
+	}
+
+}
